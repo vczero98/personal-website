ebda20584275c6b556abbbf887c55b2ac1fedd71
Vilmos Czeroczky <vxc653@cs.bham.ac.uk>
2017-10-10 13:46:33 +0100

    polished portfolio, smaller device layout

--- js/animations.js
+++ js/animations.js
@@ -1,8 +1,11 @@
 $(".portfolio-block i").hover(function(){
-		var span = $(this).parent().siblings().eq(0);
-		span.text($(this).attr('title'));
-		span.css("opacity", 1.0);
+		if (window.innerWidth > 640) {
+			var span = $(this).parent().siblings().eq(0);
+			span.text($(this).attr('title'));
+			span.css("opacity", 1.0);
+		}
   }, function(){
 		var span = $(this).parent().siblings().eq(0);
+		span.text("");
 		span.css("opacity", 0);
 });
